633a18a50f5f135035103e268cd7609dd55261ce
Will <willwinman@gmail.com>
2019-07-30 22:52:07 -0500

    Added quiz page and admin pages. /quiz and /admin/questions.

---
+++ client/src/components/Card/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.css';
+
+const Card = (props) => {
+
+    return (
+        <div className="cardWrapper" >
+            CARD
+        </div>
+    )
+};
+
+export default Card;
