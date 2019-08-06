f3fbeeeca260dcb918145b8ad27fd34dfa4b518f
VIctor Lineros <victorlineros.com>
2019-08-05 19:36:53 -0500

    adding validation for login page

--- models/user.js
+++ models/user.js
@@ -3,11 +3,27 @@ const Schema = mongoose.Schema;
 
 // need to add validation once successfully hooked into DB
 const UserSchema = new Schema({
-  name: { type: String, allowNull: false, require: true },
-  email: { type: String, allowNull: false, required: true },
-  zipcode: { type: String, allowNull: true, required: false },
-  password: { type: String, allowNull: false, required: true },
-  fb_id: { type: String, allowNull: true, required: false }
+  name: {
+    type: String,
+    allowNull: false,
+    trim: true,
+    required: "Name is required"
+  },
+  email: {
+    type: String,
+    allowNull: false,
+    trim: true,
+    required: "Email is required",
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+  },
+  zipcode: { type: String, trim: true, allowNull: true },
+  password: {
+    type: String,
+    trim: true,
+    allowNull: false,
+    required: "Password is required"
+  },
+  fb_id: { type: String, allowNull: true }
 });
 
 const User = mongoose.model("User", UserSchema);
