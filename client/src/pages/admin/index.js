a8d3f791710c38f2b65a614103dfb84ae7d07549
Alicia <aliciagarcia61@gmail.com>
2019-08-20 16:28:59 -0500

    fixed responsive on quiz and result

--- client/src/pages/admin/index.js
+++ client/src/pages/admin/index.js
@@ -1,55 +1,46 @@
 import React from "react";
-import { Col, Container, Row } from '../../components/Grid';
-import { ToastContainer } from 'react-toastify';
-import Card from '../../components/Card';
-import Jumbotron from '../../components/Jumbotron';
+import { Col, Container, Row } from "../../components/Grid";
+import { ToastContainer } from "react-toastify";
+import Card from "../../components/Card";
+import Jumbotron from "../../components/Jumbotron";
 
 class App extends React.Component {
-  render () {
+  render() {
     return (
-        <div>
-            <Container fluid>
-                <Row>
-                    <Col size="md-12">
-                    <ToastContainer />
-                        <Jumbotron>
-                            <div className="label"></div>
-                            <div style={{"display": "inline-block"}}>
-                                <h1>Admin Console</h1>
-                            </div>
-                            <div className="label"></div>
-                       
-                        </Jumbotron>
-                        <Row>
-                            <Col size="md-6">
-                                <Card>Active
-                                </Card>
-                            </Col>
-                            <Col size="md-6">
-                                <Card>
-                                    Creative
-                                </Card>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col size="md-6">
-                                <Card>
-                                    Social
-                                </Card>
-                            </Col>
-                            <Col size="md-6">
-                                <Card>
-                                    Solo
-                                </Card>
-                            </Col>
-                        </Row>
-                      
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-      );
-  }; 
+      <div>
+        <Container fluid>
+          <Row>
+            <Col size="md-12">
+              <ToastContainer />
+              <Jumbotron>
+                <div className="label" />
+                <div style={{ display: "inline-block" }}>
+                  <h1>Admin Console</h1>
+                </div>
+                <div className="label" />
+              </Jumbotron>
+              <Row>
+                <Col size="md-6">
+                  <Card>Active</Card>
+                </Col>
+                <Col size="md-6">
+                  <Card>Creative</Card>
+                </Col>
+              </Row>
+              <Row>
+                <Col size="md-6">
+                  <Card>Social</Card>
+                </Col>
+                <Col size="md-6">
+                  <Card>Solo</Card>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
